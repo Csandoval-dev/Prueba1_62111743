81cb00302f06af0724d7e308a897c0987fb9e2bc
Csandoval-dev <156733491+Csandoval-dev@users.noreply.github.com>
2025-08-11 21:36:59 -0600

    Creacion de componentes

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,85 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+  import { ref, onMounted, watch } from 'vue'
+  import { db } from './data/articulos'
+  import Articulo from './components/Articulo.vue'
+  import Header from './components/Header.vue'
+  import Footer from './components/Footer.vue'
+
+  const articulos = ref([])
+  const carrito = ref([])
+  const articulo = ref({})
+
+  watch(carrito, () => {
+    guardarLocalStorage()
+  }, {
+    deep: true
+  })
+
+  const guardarLocalStorage = () => {
+    localStorage.setItem('carrito', JSON.stringify(carrito.value))
+  }
+
+  onMounted(() => {
+    articulos.value = db
+    articulo.value = db[0]
+  })
+
+  const agregarCarrito = (articulo) => {
+    const existeCarrito = carrito.value.findIndex(producto => 
+      producto.id === articulo.id
+    )
+    
+    if (existeCarrito >= 0) {
+      carrito.value[existeCarrito].cantidad++
+    } else {
+      articulo.cantidad = 1
+      carrito.value.push(articulo)
+    }
+    guardarLocalStorage()
+  }
+
+  const decrementarCantidad = (id) => {
+    const index = carrito.value.findIndex(producto => producto.id === id)
+    if (carrito.value[index].cantidad <= 1) return 
+    carrito.value[index].cantidad--
+  }
+
+  const incrementarCantidad = (id) => {
+    const index = carrito.value.findIndex(producto => producto.id === id)
+    carrito.value[index].cantidad++
+  }
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+   <!-- Renderización del Header -->
+   <Header 
+     :carrito="carrito"
+     :articulo="articulo"
+     @decrementar-cantidad="decrementarCantidad"
+     @incrementar-cantidad="incrementarCantidad"
+     @agregar-carrito="agregarCarrito" 
+   />
+
+  <main class="container-xl mt-5">
+    <h2 class="text-center">Nuestra Colección</h2>
+    <div class="row mt-5">
+      <!-- Renderización de Artículos -->
+      <Articulo 
+        v-for="articulo in articulos"
+        :key="articulo.id"
+        :articulo="articulo" 
+        @agregar-carrito="agregarCarrito"
+      />
+    </div>
+  </main>
+
+  <!-- Renderización del Footer -->
+  <Footer/>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+h1 {
+  text-transform: uppercase;
+  color: green;
 }
 </style>
---
+++ src/components/Articulo.vue
@@ -0,0 +1,36 @@
+<script setup>
+    const props = defineProps({
+        articulo: {
+            type: Object,
+            required: true
+        }
+    })
+
+    defineEmits(['agregar-carrito'])
+</script>
+
+<template>
+    <div class="col-md-6 col-lg-4 my-4 row align-items-center">
+        <div class="col-4">
+            <img 
+                class="img-fluid" 
+                v-bind:src="'/img/' + articulo.imagen + '.jpg'" 
+                :alt="'imagen articulo ' + articulo.nombre">
+        </div>
+        <div class="col-8">
+            <h3 class="text-black fs-4 fw-bold text-uppercase">{{ articulo.nombre }}</h3>
+           
+            <p>{{ articulo.descripcion }}</p>
+            <p class="fw-black text-primary fs-3">${{ articulo.precio }}</p>
+            <button 
+                type="button"
+                class="btn btn-dark w-100"
+                @click="$emit('agregar-carrito', articulo)"
+            >Agregar al Carrito</button>
+        </div>
+    </div><!-- FIN ARTICULO -->
+</template>
+
+<style scoped>
+
+</style>
